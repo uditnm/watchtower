ba2c7d2d06cbb72a9e0e3244c2fdd49779a1d8b2
uditnm <udit20001@gmail.com>
2026-09-02 16:25:57 +0530

    feat: add monitor model and migration

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,7 @@ from app.database import engine
 
 from app.models.base import Base
 from app.models.user import User
+from app.models.monitor import Monitor
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
---
+++ app/models/monitor.py
@@ -0,0 +1,26 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import JSON, DateTime, FetchedValue, ForeignKey, Enum, text
+from datetime import datetime
+from enum import Enum as PyEnum
+
+class MonitorStatus(PyEnum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+
+class Monitor(Base):
+    __tablename__ = "monitor"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    config: Mapped[dict] = mapped_column(JSON, nullable=False)
+    last_change_detected_at: Mapped[datetime | None] = mapped_column(DateTime)
+    next_run_at: Mapped[datetime] = mapped_column(DateTime, nullable = False)
+    status: Mapped[MonitorStatus] = mapped_column(Enum(MonitorStatus), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable = False, server_default=text("CURRENT_TIMESTAMP"))
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, 
+        nullable = False, 
+        server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"), 
+        server_onupdate=FetchedValue()
+    )
